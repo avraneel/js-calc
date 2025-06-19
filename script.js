350d38c555c1e2ef070108defe7dddd44c5f1168
Avraneel Pal <avraneelpal22@gmail.com>
2025-06-19 22:02:41 +0530

    Add Operation on display and add clear screen functionality

--- script.js
+++ script.js
@@ -1,20 +1,73 @@
 "use strict";
 
+let operation = "";
+const arithOperationSet = ["+", "-", "*", "/"];
+
+// adding listener for each num buttons
 const numButtonList = document.querySelectorAll(".num-button");
 let numstring = "";
 
-// adding listener for each num buttons
 for (const numButton of numButtonList) {
     numButton.addEventListener("click", () => {
         const num = numButton.textContent;
-        setNum(num);
+        displayNumOnScreen(num);
     });
 }
 
-function setNum(num) {
+function displayNumOnScreen(num) {
     const display = document.querySelector(".display");
     /* if we press multiple buttons, then it will build a string and then 
         display it */
-    numstring += num;
-    display.textContent = numstring;
+    //numstring += num;
+    display.textContent += num;
 }
+
+const opButtonList = document.querySelectorAll(".op-button");
+
+// adding listener for each operation button
+for(const opButton of opButtonList) {
+    opButton.addEventListener("click", function () {
+        const op = opButton.textContent;
+        if(arithOperationSet.includes(op)) {
+            // Operation is one of the 4 arithmetic operations
+            console.log("arithmetic op found");
+            displayOperationOnScreen(op);
+            operation = op;
+        }
+        else if(op === "C") {
+            clearScreen();
+        }
+        else if(op === "=") {
+            evalulate();
+        }
+        console.log(op);
+    });
+}
+
+function displayOperationOnScreen(op) {
+    const display = document.querySelector(".display");
+    display.textContent += " " + op;
+}
+
+function clearScreen() {
+    const display = document.querySelector(".display");
+    display.textContent = " ";
+}
+
+function evalulate() {
+    switch (operation) {
+    case "+":
+        console.log("add"); 
+        break;
+    case "-":
+        break;
+    case "*":
+        break;
+    case "/":
+        break;
+    case "C":
+        clearScreen();
+        break;
+    }
+}
+
